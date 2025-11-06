5a50d631078b4e5a70df5affbb3de1d1827ff0db
Rodrigo A. Quezada Ccahuana <danter9462@gmail.com>
2025-11-06 12:17:33 -0500

    cambio-jueves5-backend

--- src/main/java/com/saludvida/api/repository/HorarioRepository.java
+++ src/main/java/com/saludvida/api/repository/HorarioRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface HorarioRepository extends JpaRepository<Horario, Integer> {
-
+    
     // Este método ya existía
     List<Horario> findByMedico(Medico medico);
 
