448509f612cf9518f00285735b9855f4729e0424
Rodrigo A. Quezada Ccahuana <danter9462@gmail.com>
2025-11-06 20:24:06 -0500

    cambio-jueves18

--- src/main/java/com/saludvida/api/config/JwtAuthenticationFilter.java
+++ src/main/java/com/saludvida/api/config/JwtAuthenticationFilter.java
@@ -57,7 +57,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String getTokenFromRequest(HttpServletRequest request) {
         final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-
         if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
             return authHeader.substring(7);
         }
